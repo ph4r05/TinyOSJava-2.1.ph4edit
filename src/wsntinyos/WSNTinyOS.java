21b1d88ffdc334307471a07fadfcaeeb2961a8ea
ph4r05 <ph4r05@ph4.maleus>
2012-04-20 10:34:45 +0200

    initial import for tinyos library

---
+++ src/wsntinyos/WSNTinyOS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsntinyos;
+
+/**
+ *
+ * @author ph4r05
+ */
+public class WSNTinyOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
